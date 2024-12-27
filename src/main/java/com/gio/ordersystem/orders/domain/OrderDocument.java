59940e799e7f075cb7645d17a60e4b01ada9f1df
kwongio <rldh11111@naver.com>
2024-12-27 18:57:48 +0900

    feat: elasticsearch 타입 추가

--- src/main/java/com/gio/ordersystem/orders/domain/OrderDocument.java
+++ src/main/java/com/gio/ordersystem/orders/domain/OrderDocument.java
@@ -4,7 +4,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.time.LocalDateTime;
 
@@ -15,10 +18,20 @@ public class OrderDocument {
 
     @Id
     private String id; // Elasticsearch는 기본적으로 String ID 사용
+
+    @Field(type = FieldType.Long)
     private Long userId;
+
+    @Field(type = FieldType.Long)
     private Long productId;
+
+    @Field(type = FieldType.Integer)
     private int quantity;
+
+    @Field(type = FieldType.Long)
     private long totalPrice;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
     private LocalDateTime orderDate;
 
     @Builder
